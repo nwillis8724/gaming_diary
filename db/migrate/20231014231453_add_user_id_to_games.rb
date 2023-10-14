61e86997f81905222bf9227e7b61c36b77e09ad6
noah willis <n.willis.8724@gmail.com>
2023-10-14 18:21:32 -0500

    change schema

--- db/migrate/20231014231453_add_user_id_to_games.rb
+++ db/migrate/20231014231453_add_user_id_to_games.rb
@@ -1,5 +1,5 @@
 class AddUserIdToGames < ActiveRecord::Migration[6.1]
   def change
-    add_reference :games, :user, null: false, foreign_key: true
+    add_column :games, :user_id, :integer
   end
 end
--- db/migrate/20231014231456_add_user_id_to_comments.rb
+++ db/migrate/20231014231456_add_user_id_to_comments.rb
@@ -1,5 +1,5 @@
 class AddUserIdToComments < ActiveRecord::Migration[6.1]
   def change
-    add_reference :comments, :user, null: false, foreign_key: true
+    add_column :comments, :user_id, :integer
   end
 end
