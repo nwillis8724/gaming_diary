0e72ab1dfa556353416c1fd78cec9b67e6dfc7ca
noah willis <n.willis.8724@gmail.com>
2023-10-14 18:01:56 -0500

    fix credentials

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,24 @@
 class GamesController < ApplicationController
+
+    def index
+        games = Game.all
+        render json: games
+    end
+
+    def show
+        game = Game.find(params[:id])
+        render json: game
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
 end
