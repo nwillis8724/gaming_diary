e231bddde0b69983cb9b733cbb6a1bdade5ac256
noah willis <n.willis.8724@gmail.com>
2023-10-20 19:02:37 -0500

    temp css added

--- client/src/components/GameDisplay.js
+++ client/src/components/GameDisplay.js
@@ -17,24 +17,24 @@ function GameDisplay() {
         <div class="App">
             {games.map((game, i) =>{
                 return(
-                    <div id="game_card" key={i}>
+                    <div class="game_card" key={i}>
                         <button> x </button>
-                        <h1 id="game_title">{game.title}</h1>
-                        <img id="game_image" src={game.image} alt={game.title}/>
-                        <h2>Platform: {game.platform}</h2>
-                        <h3>Genre: {game.genre}</h3>
-                            <div id="comment_section">
-                                {game.comments && game.comments.length > 0 ? (
-                                    game.comments.map((comment, i) => (
-                                        <div key={i}>
-                                            <p>{comment.text}</p>
-                                            <p>{comment.rating}/5</p>
-                                        </div>
-                                    ))
-                                ) : (
-                                    <p>No comments yet.</p>
-                                )}
-                            </div>
+                        <h1 class="game_card_text" id="game_title">{game.title}</h1>
+                        <img class="game_image" src={game.image} alt={game.title}/>
+                        <h2 class="game_card_text">Platform: {game.platform}</h2>
+                        <h3 class="game_card_text">Genre: {game.genre}</h3>
+                        <div class="comment_section">
+                            {game.comments && game.comments.length > 0 ? (
+                                game.comments.map((comment, i) => (
+                                    <div class="comment" key={i}>
+                                        <p>{comment.text}</p>
+                                        <p>{comment.rating}/5</p>
+                                    </div>
+                                ))
+                            ) : (
+                                <p>No comments yet.</p>
+                            )}
+                        </div>
                     </div>
                 )
             })}
